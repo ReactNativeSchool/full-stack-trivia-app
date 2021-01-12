ba57d753746a77fb2a8b57aa21b8dec91fd0f7a6
Spencer Carli <spencercarli@gmail.com>
2021-01-12 11:21:37 -0600

    Reorg

--- trivia-app-server/pages/api/graphql.ts
+++ trivia-app-server/pages/api/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "./schemas";
-import { resolvers } from "./resolvers";
+import { typeDefs } from "../../graphql/schemas";
+import { resolvers } from "../../graphql/resolvers";
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
